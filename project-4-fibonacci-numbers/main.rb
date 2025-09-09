89bdfe05289a00a42634a1ac7ae035e4144b8a3a
NKDuy <nkduy.dev@gmail.com>
2025-09-09 15:13:57 +0700

    Project 4 - Fibonacci Numbers

---
+++ project-4-fibonacci-numbers/main.rb
@@ -0,0 +1,15 @@
+# 1 1 2 3 5 8 13 21 34 55 ...
+
+print "Enter a number: "
+number = gets.chomp.to_i
+
+fibonacciPrev = 0
+fibonacci = 1
+
+while fibonacci <= number
+  puts fibonacci
+  # temp = fibonacci
+  # fibonacci = fibonacci + fibonacciPrev
+  # fibonacciPrev = temp
+  fibonacciPrev, fibonacci = fibonacci, fibonacci + fibonacciPrev
+end
