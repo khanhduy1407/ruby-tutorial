76d2dc78f05885e2ad076c48588683df8c4dfdec
NKDuy <nkduy.dev@gmail.com>
2025-09-10 09:35:14 +0700

    Files and Directories

---
+++ 8-files/files_1.rb
@@ -0,0 +1,30 @@
+# puts Dir.pwd
+
+# Dir.mkdir "foo"
+# Dir.mkdir "bar"
+
+# print Dir.glob "*"
+# print Dir.glob "*.rb"
+
+# Dir.glob("*") do |filename|
+#   puts filename
+# end
+
+# puts Dir.pwd
+# Dir.chdir "8-files"
+# Dir.chdir "D:/Workspace/Learning/ruby-tutorial/8-files"
+# puts Dir.pwd
+
+# Dir.delete("bar")
+
+# puts Dir.pwd
+# folder = Dir.open "D:/Workspace/Learning/ruby-tutorial/8-files/foo"
+# folder.each do |file|
+#   puts file
+# end
+
+# print Dir.entries("D:/")
+# Dir.foreach("D:/") do |entry|
+#   puts entry
+# end
+# print Dir["D:/*"]
