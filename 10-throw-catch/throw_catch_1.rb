20601f468b4ca5db7946a1aa1bea63c50cc6f0ca
NKDuy <nkduy.dev@gmail.com>
2025-09-10 10:56:49 +0700

    Throw Catch Statements

---
+++ 10-throw-catch/throw_catch_1.rb
@@ -0,0 +1,19 @@
+array = [
+  ["foo", "bar", "wanted", "test"],
+  [1,2,3,4,5],
+  ['a', 'b', 'c', 'd']
+]
+
+counter = 0
+search = nil
+search = catch(:found) do
+  array.each do |row|
+    row.each do |item|
+      counter += 1
+      throw(:found, item) if item == "wanted"
+    end
+  end
+end
+
+puts "Counter : #{counter}"
+puts "Search  : #{search}"
